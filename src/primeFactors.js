114fe25ebdb4bf4853ff5e259788a7d12b1da170
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-02-20 16:49:17 +0000

    Add test and code for integer 2

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -1,5 +1,7 @@
-function primeFactors() {
-  return []
+function primeFactors(n) {
+  let factors = []
+  if (n > 1) factors.push(2)
+  return factors
 }
 
 module.exports = primeFactors
--- src/primeFactors.test.js
+++ src/primeFactors.test.js
@@ -4,4 +4,8 @@ describe("primeFactors", () => {
   it("should return [] if given 1", () => {
     expect(primeFactors(1)).toEqual([])
   })
+
+  it("should return [2] if given 2", () => {
+    expect(primeFactors(2)).toEqual([2])
+  })
 })
